6d5f0e32572460f883e6d868269e2c5b29eff4be
Filip Blaauw <filipbl@gmail.com>
2016-10-01 15:26:18 +0200

    Meta updates
    Change share image and added login link to page if not logged in

--- client/config/config.js
+++ client/config/config.js
@@ -4,14 +4,14 @@ Router.plugin('seo', {
     suffix: 'Sangbasen',
     separator: '|',
     description: 'Noter og tabs til folket!',
-    image: 'http://fiskelogg.no/img/share.png',
+    image: 'http://sangbasen.blaauw.me/sangbasen.png',
     meta: {
       keywords: ['noter', 'tabs', 'musikk', 'lovsang']
     },
     og: {
       site_name: 'Sangbasen',
       type: 'article',
-      image: 'http://fiskelogg.no/img/share.png',
+      image: 'http://sangbasen.blaauw.me/sangbasen.png',
       description: 'Noter og tabs til folket!'
       // etc.
     }
