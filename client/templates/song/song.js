a95b0a431b1856d40d0da7ea28853b5146b89fc3
Filip Blaauw <filipbl@gmail.com>
2016-10-15 13:11:25 +0200

    Implementation of chordpro and Dropbox format to song template.
    Added chordpro
    Removed old <pre> tag
    Added Dropbox button
    Adjustments to song template

--- client/templates/song/song.js
+++ client/templates/song/song.js
@@ -7,6 +7,9 @@ Template.Song.events({
   'click #printChords': function () {
     window.print();
   },
+  'click #chordproButton': function () {
+    $('#chordpro').modal('show');
+  },
   "click [data-action='addLikes']": function (event) {
     event.preventDefault();
     var song = Songs.findOne({_id: this._id});
@@ -86,6 +89,106 @@ Template.Song.helpers({
 });
 
 Template.Song.onRendered(function() {
-  $("pre").transpose();
+
+  // Generate .chordpro file for download
+  function downloadInnerHtml(filename, elId, mimeType) {
+    var data = Songs.findOne({slug: Router.current().params.slug});
+    var elHtml = '{title: '+data.title + '}\n' + '{artist: '+data.artist + '}\n' + '{key: '+data.key + '}\n' + '{bpm: '+data.bpm + '}\n' + '{time: '+data.time + '}\n\n' + data.chords;
+    var link = document.createElement('a');
+    mimeType = mimeType || 'text/plain';
+
+    link.setAttribute('download', filename);
+    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+    link.click();
+  }
+
+  var fileName =  this.data.title + '.chordpro';
+
+  $('#download-chordpro').click(function(){
+      downloadInnerHtml(fileName, 'test','text/plain');
+  });
+
+
+  // Create a Dropbox button
+  $.getScript("https://www.dropbox.com/static/api/2/dropins.js",function(){
+    Dropbox.init({ appKey: 'p5b0bu8fra0q7vf' });
+  });
+
   $('.button').popup();
+
+  // Only do chordpro parsing if song container has content
+  if ($('#song-container').length){
+    // Set active button based on original key
+    $(".pagination").find("[data-key='" + this.data.key + "']").addClass("active");
+
+    // Find chords and key from loaded song
+    var data = this.data.chords;
+    var key = this.data.key;
+
+    // Initial parsing of chords
+    var $song = $(chordpro.parse(data).render());
+    $('#song-container').append($song);
+    $song.find('td > span.song-text-segment').each(function (i) {
+    	var textWidth = $(this).width();
+    	var chordWidth = $(this).parent().width();
+    	var nextSpan = $song.find('td > span.song-text-segment')[i + 1];
+        // Check next text is on the same row
+        if (nextSpan && $(nextSpan).parent().parent()[0] === $(this).parent().parent()[0]) {
+            // The next text is on the same row
+            var thisText = $(this).html();
+            var nextText = $(nextSpan).html();
+            var stretched = thisText.trim().length > 0 && textWidth < chordWidth;
+            var splitWord = thisText.indexOf('&nbsp;') <= 0 && nextText.indexOf('&nbsp;') !== 0;
+            // If this column has been stretched by the chord,
+            // and the text does not end with a space
+            // and next text does not start with a space
+            // then insert a hyphen.
+            if (stretched && splitWord) {
+                $(this).append('&nbsp;-&nbsp;');
+            }
+        }
+    });
+
+    // Transposing from button click
+    $(".transpose").click(function () {
+
+      // Change active state on clicked button
+      $(".transpose").removeClass("active");
+      $(this).addClass('active');
+
+
+      // Load data from current route
+      var data = Songs.findOne({slug: Router.current().params.slug});
+      var chords = data.chords;
+      var key = $(this).attr('data-key');
+      var $song = $(chordpro.parse(chords).render(key));
+
+      // Re-run the parsing
+      $('#song-container').html($song);
+      $song.find('td > span.song-text-segment').each(function (i) {
+      	var textWidth = $(this).width();
+      	var chordWidth = $(this).parent().width();
+      	var nextSpan = $song.find('td > span.song-text-segment')[i + 1];
+          // Check next text is on the same row
+          if (nextSpan && $(nextSpan).parent().parent()[0] === $(this).parent().parent()[0]) {
+              // The next text is on the same row
+              var thisText = $(this).html();
+              var nextText = $(nextSpan).html();
+              var stretched = thisText.trim().length > 0 && textWidth < chordWidth;
+              var splitWord = thisText.indexOf('&nbsp;') <= 0 && nextText.indexOf('&nbsp;') !== 0;
+              // If this column has been stretched by the chord,
+              // and the text does not end with a space
+              // and next text does not start with a space
+              // then insert a hyphen.
+              if (stretched && splitWord) {
+                  $(this).append('&nbsp;-&nbsp;');
+              }
+          }
+      });
+      $('#key').html(key);
+
+    });
+  }
+
+
 });
