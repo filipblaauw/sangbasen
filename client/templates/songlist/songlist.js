c1e7a65c7a17d06145e433a66410188bb18d68c0
Filip Blaauw <filipbl@gmail.com>
2017-11-17 21:41:45 +0100

    Raise limit to 10 songs on frontpage

--- client/templates/songlist/songlist.js
+++ client/templates/songlist/songlist.js
@@ -4,13 +4,13 @@ Template.songList.helpers({
   },
   favorites: function() {
     var user = Meteor.user()._id;
-    return Songs.find({voters: user}, {limit: 5});
+    return Songs.find({voters: user}, {limit: 10});
   },
   mostLiked: function() {
-    return Songs.find({likes: { $gt: 0 }}, {limit: 5, sort: {likes: "desc"}});
+    return Songs.find({likes: { $gt: 0 }}, {limit: 10, sort: {likes: "desc"}});
   },
   latest: function() {
-    return Songs.find({}, {sort: {createdAt: -1}, limit: 5});
+    return Songs.find({}, {sort: {createdAt: -1}, limit: 10});
   }
 });
 
